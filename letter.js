b681786dfbd54ba550e38f94842aa834ab5fbf43
Brett Cimbalik <bcimbali@gmail.com>
2018-04-26 21:31:49 -0500

    got letter file working

---
+++ letter.js
@@ -0,0 +1,25 @@
+
+
+var Letter = function(alpha) {
+    this.letter = alpha;
+    this.letterGuessed = false
+    this.displayLetter = function() {
+        if (this.letterGuessed === true) {
+            return this.letter;
+        }
+        else {
+            return '_';
+        }
+    }
+    this.checkLetter = function(guess) {
+        if (guess === this.letter) {
+            this.letterGuessed = true;
+        }
+    }
+}; 
+
+
+// var testLetter = new Letter('s');
+// console.log(testLetter.displayLetter());
+
+module.exports = Letter;
